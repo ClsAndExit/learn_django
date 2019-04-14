210b67b6aa6770c14536a5347f3a5249cb14c415
Mr.Zhang <zhangmiao.gm.cn@gmail.com>
2019-04-14 14:13:51 +0800

    create learn_app

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from learn_app import views as learn_views
 
 urlpatterns = [
+    path('', learn_views.index),
     path('admin/', admin.site.urls),
 ]
