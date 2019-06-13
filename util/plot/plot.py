0a6de2832ae086fcf0c4d3cc3a4e57ba425f6487
ClsAndExit <zhang_0x01@163.com>
2019-06-13 21:25:12 +0800

    add

--- util/plot/plot.py
+++ util/plot/plot.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from pylab import *
 
+fonts = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=12)
+fonts1 = FontProperties(fname=r"C:\Windows\Fonts\times.ttf", size=12)
+
 
 ##C:\Windows\Fonts\simsun.ttc
 def zhexian():
     start = 50  # 起始特征数量
     feature = 500  # 选取特征词个数
     length = 50  # 每次增长特征数量
-
-
     # 指定图形的字体 fontdict=myfont
     myfont = {'family': 'Times New Roman',
               'color': 'darkred',
@@ -22,9 +23,8 @@ def zhexian():
               'size': 20,
               }
 
-    fonts = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=12)
-    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-    # mpl.rc('font', family='Times New Roman', size=14)
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    mpl.rc('font', family='Times New Roman', size=12)
 
     # plt.title("NB")
     plt.xlabel('主题数K', fontsize=12, color='black',fontproperties=fonts)
@@ -32,8 +32,6 @@ def zhexian():
     plt.title("不同主题数K对应的F1值",fontproperties=fonts)
 
     # 画网格
-
-
     # 将x主刻度标签设置为feature/10的倍数(也即以feature/10为主刻度单位其余可类推)
     xmajorLocator = MultipleLocator(feature / 10);
     # 设置x轴标签文本的格式
@@ -60,8 +58,8 @@ def zhexian():
 
     # 折线图
     # plt.scatter(np.arange(0,len(scores)),scores)  #散点图 plt.bar()条形图
-    plot(t, scores1, label='传统LDA', marker='o', color='#FFD700', linewidth="2")
-    plot(t, scores2, label='改进LDA', marker='*', color='#87CEFF', linewidth="2")
+    plot(t, scores1, label='LDA', marker='o', color='#FFD700', linewidth="2")
+    plot(t, scores2, label='Huffman-LDA', marker='*', color='#87CEFF', linewidth="2")
 
     # 调节轴之间的间距和轴与边框之间的距离
     plt.subplots_adjust(left=.13)
@@ -70,58 +68,71 @@ def zhexian():
     plt.subplots_adjust(bottom=.12)
 
     # plt.legend(ncol=2,loc=8)  #ncol=2显示两列,显示线条label
-    plt.legend(ncol=2, loc=3,prop=fonts)  ##cmr10:宋体，cmb10：timesnewroman
+    plt.legend(ncol=2, loc=3,prop=fonts1)  ##cmr10:宋体，cmb10：timesnewroman
     plt.show()
 
 
-def zhuzhaungtu():
+def zhuzhaungtu1_1():
 
-
-    # plt.figure(figsize=(6, 6))  # 调用figure创建一个绘图对象，宽度和高度，单位为英寸
-    fonts = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=12)
-    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    mpl.rc('font', family='Times New Roman', size=12)
     # X轴数据
 
+    num_list = [0.891,0.895,0.903,0.879,0.915]
 
-    '''
-    0.671814825	0.63671336	0.691817955
-0.7586887326‬	0.763892954	0.869823903
-0.8969813836‬	0.868701652	0.902875679
+    num_list2 = [0.827,0.855,0.872,0.814,0.834]
 
-0.896‬	0.868	0.902
-0.843	0.878	0.822
+    # X轴标签
+    classifier = ('互联网', '商业', '教育', '娱乐', '体育')
 
-    '''
+    indices = np.arange(len(num_list))
+    rects1 = plt.bar(indices - .15, num_list, .3, label="Huffman-LDA+SVM",
+                     color='navy')  # 这里是产生横向柱状图 barh h--horizontal，bar纵向
+    rects2 = plt.bar(indices + .15 , num_list2, .3, label="Huffman-LDA+NB", color='c')
+
+    plt.ylim(ymax=1.0, ymin=0.5)
+    plt.yticks(np.arange(0.5, 1.05, 0.05))
+    plt.ylabel("精确率",fontproperties=fonts)  # X轴标签
+
+    x_pos = np.arange(len(classifier))
+    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts)
+    plt.title("应用不同分类器得出各类别精确率",fontproperties=fonts)
+    plt.legend(loc='best', fontsize=10,prop=fonts1)  # 参数loc=表示图例放置的位置，左右上角
+    plt.subplots_adjust(left=.12)  # 调节轴之间的间距和轴与边框之间的距离
+    plt.subplots_adjust(right=.97)
+    plt.subplots_adjust(top=.97)
+    plt.subplots_adjust(bottom=.06)
+
+    plt.grid(True, axis='y', color="Black",
+             linewidth="0.15")  # grid 表示是否显示图轴网格,b=None|True|False|on|off,是否开启, which=’major’，minor|both 选择主、次网格开启方式, axis=’both’，x|y 选择使用网格的数轴  #696969
+
+    plt.show()
+
+def zhuzhaungtu1_2():
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    mpl.rc('font', family='Times New Roman', size=12)
+    # X轴数据
     num_list = [0.896,0.868,0.902]
 
     num_list2 = [0.843,0.878,0.822]
 
 
-    # X轴标签  LR 、KNN、NB、RF、SVM
+    # X轴标签
     classifier = ('Macro-P', 'Macro-R', 'Macro-F1')
 
     indices = np.arange(len(num_list))
-    rects1 = plt.bar(indices - .15, num_list, .3, label="SVM",
+    rects1 = plt.bar(indices - .15, num_list, .3, label="Huffman-LDA+SVM",
                      color='navy')  # 这里是产生横向柱状图 barh h--horizontal，bar纵向
-    rects2 = plt.bar(indices + .15 , num_list2, .3, label="NB", color='c')
-    #rects3 = plt.bar(indices + .3, num_list3, .3, label="改进LDA", color='#FFD700')
-
-    # 在柱状图上加数字
-    # for rect in rects1:
-    #     height = rect.get_height()
-    #     plt.text(rect.get_x() + rect.get_width() / 2, height, str(height) + '%', ha='center', va='bottom')
-    # for rect in rects2:
-    #     height = rect.get_height()
-    #     plt.text(rect.get_x() + rect.get_width() / 2, height, str(height) + '%', ha='center', va='bottom')
+    rects2 = plt.bar(indices + .15 , num_list2, .3, label="Huffman-LDA+NB", color='c')
 
     plt.ylim(ymax=1.0, ymin=0.5)
     plt.yticks(np.arange(0.5, 1.05, 0.05))
     plt.ylabel("宏平均值",fontproperties=fonts)  # X轴标签
 
     x_pos = np.arange(len(classifier))
-    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts)
-    plt.title("LDA主题模型结合不同分类器对应的宏平均值",fontproperties=fonts)
-    plt.legend(loc='best', fontsize=10,prop=fonts)  # 参数loc=表示图例放置的位置，左右上角
+    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts1)
+    plt.title("应用不同分类器对应的宏平均值",fontproperties=fonts)
+    plt.legend(loc='best', fontsize=10,prop=fonts1)  # 参数loc=表示图例放置的位置，左右上角
     plt.subplots_adjust(left=.12)  # 调节轴之间的间距和轴与边框之间的距离
     plt.subplots_adjust(right=.97)
     plt.subplots_adjust(top=.97)
@@ -132,60 +143,69 @@ def zhuzhaungtu():
 
     plt.show()
 
-def zhuzhaungtu2():
 
-
-    # plt.figure(figsize=(6, 6))  # 调用figure创建一个绘图对象，宽度和高度，单位为英寸
-    fonts = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=12)
-    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+def zhuzhaungtu2_1():
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    mpl.rc('font', family='Times New Roman', size=12)
     # X轴数据
 
+    num_list = [0.748,0.636,0.563,0.673,0.736]
+    num_list2 = [0.801,0.695,0.701,0.777,0.817]
+    num_list3=[0.891,0.895,0.903,0.879,0.915]
 
-    '''
-    0.748	0.636	0.563	0.673	0.736
-0.801	0.695	0.701	0.777	0.817
-0.891	0.895	0.903	0.879	0.915
+    # X轴标签  LR 、KNN、NB、RF、SVM
+    classifier = ('互联网', '商业', '教育', '娱乐', '体育')
 
+    indices = np.arange(len(num_list))
+    rects1 = plt.bar(indices - .3, num_list, .3, label="SVM",
+                     color='navy')  # 这里是产生横向柱状图 barh h--horizontal，bar纵向
+    rects2 = plt.bar(indices , num_list2, .3, label="LDA+SVM", color='c')
+    rects3 = plt.bar(indices + .3, num_list3, .3, label="Huffman-LDA+SVM", color='#FFD700')
 
-0.671	0.636	0.691
-0.758	0.763	0.869
-0.896	0.868	0.902
+    plt.ylim(ymax=1.0, ymin=0.5)
+    plt.yticks(np.arange(0.5, 1.05, 0.05))
+    plt.ylabel("精确率",fontproperties=fonts)  # X轴标签
 
-    '''
-    # num_list = [0.748,0.636,0.563,0.673,0.736]
-    # num_list2 = [0.801,0.695,0.701,0.777,0.817]
-    # num_list3=[0.891,0.895,0.903,0.879,0.915]
+    x_pos = np.arange(len(classifier))
+    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts)
+    plt.title("不同类别精确率",fontproperties=fonts)
+    plt.legend(loc='best', fontsize=10,prop=fonts1)  # 参数loc=表示图例放置的位置，左右上角
+    plt.subplots_adjust(left=.12)  # 调节轴之间的间距和轴与边框之间的距离
+    plt.subplots_adjust(right=.97)
+    plt.subplots_adjust(top=.97)
+    plt.subplots_adjust(bottom=.06)
+
+    plt.grid(True, axis='y', color="Black",
+             linewidth="0.15")  # grid 表示是否显示图轴网格,b=None|True|False|on|off,是否开启, which=’major’，minor|both 选择主、次网格开启方式, axis=’both’，x|y 选择使用网格的数轴  #696969
+
+    plt.show()
+
+def zhuzhaungtu2_2():
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    mpl.rc('font', family='Times New Roman', size=12)
+    # X轴数据
     num_list = [0.671,0.636,0.691]
     num_list2 = [0.758,0.763,0.869]
     num_list3=[0.896,0.868,0.902]
 
-
-    # X轴标签  LR 、KNN、NB、RF、SVM
-    # classifier = ('互联网', '商业', '教育', '娱乐', '体育')
+    # X轴标签
     classifier = ('Macro-P', 'Macro-R', 'Macro-F1')
 
     indices = np.arange(len(num_list))
-    rects1 = plt.bar(indices - .3, num_list, .3, label="不使用LDA",
+    rects1 = plt.bar(indices - .3, num_list, .3, label="SVM",
                      color='navy')  # 这里是产生横向柱状图 barh h--horizontal，bar纵向
-    rects2 = plt.bar(indices , num_list2, .3, label="传统LDA", color='c')
-    rects3 = plt.bar(indices + .3, num_list3, .3, label="改进LDA", color='#FFD700')
+    rects2 = plt.bar(indices , num_list2, .3, label="LDA+SVM", color='c')
+    rects3 = plt.bar(indices + .3, num_list3, .3, label="Huffman-LDA+SVM", color='#FFD700')
 
-    # 在柱状图上加数字
-    # for rect in rects1:
-    #     height = rect.get_height()
-    #     plt.text(rect.get_x() + rect.get_width() / 2, height, str(height) + '%', ha='center', va='bottom')
-    # for rect in rects2:
-    #     height = rect.get_height()
-    #     plt.text(rect.get_x() + rect.get_width() / 2, height, str(height) + '%', ha='center', va='bottom')
 
     plt.ylim(ymax=1.0, ymin=0.5)
     plt.yticks(np.arange(0.5, 1.05, 0.05))
     plt.ylabel("宏平均值",fontproperties=fonts)  # X轴标签
 
     x_pos = np.arange(len(classifier))
-    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts)
+    plt.xticks(x_pos, classifier, fontsize=12,fontproperties=fonts1)
     plt.title("使用LDA主题模型对应的宏平均值",fontproperties=fonts)
-    plt.legend(loc='best', fontsize=10,prop=fonts)  # 参数loc=表示图例放置的位置，左右上角
+    plt.legend(loc='best', fontsize=10,prop=fonts1)  # 参数loc=表示图例放置的位置，左右上角
     plt.subplots_adjust(left=.12)  # 调节轴之间的间距和轴与边框之间的距离
     plt.subplots_adjust(right=.97)
     plt.subplots_adjust(top=.97)
@@ -196,7 +216,9 @@ def zhuzhaungtu2():
 
     plt.show()
 
-
 if __name__ == '__main__':
 # zhexian()
-    zhuzhaungtu()
+# zhuzhaungtu1_1() #图4-8
+    zhuzhaungtu1_2() #图4-9
+    #zhuzhaungtu2_1()  #图4-6
+# zhuzhaungtu2_2() #图4-7
